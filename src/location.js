944a30ccac610a5a9e1c9f51f9a7799e98dc0421
Peter Gyory <ptg3662@rit.edu>
2016-08-27 16:06:43 -0700

    Add create markers function

--- src/location.js
+++ src/location.js
@@ -1,7 +1,15 @@
 import GoogleMapLoader from 'google-maps';
 import config from './config';
+import { forEach } from 'ramda';
 
 let map;
+let googleObj;
+
+// basement coords.json = { latitude: 37.787487600000006, longitude: -122.3965566 }
+const tempSoundMarkers = [
+  { lat: 37.787487600000006, lng: -122.3965566 },
+  { lat: 37.78748760000003, lng: -122.39658 }
+]
 
 const setMapLocation = ({ maps }) => {
   if (navigator.geolocation) {
@@ -11,11 +19,20 @@ const setMapLocation = ({ maps }) => {
         ({ coords }) => map.setCenter(new maps.LatLng(coords.latitude, coords.longitude))
       );
   }
-}
+};
+
+export const createWispMarkers = forEach(
+  ({ lat, lng }) => new googleObj.maps.Marker({
+      position: { lat, lng },
+      map,
+    })
+);
 
 // init google maps
 GoogleMapLoader.KEY = config.gmaps.apiKey;
 GoogleMapLoader.load(google => {
+  googleObj = google;
   map = new google.maps.Map(document.querySelector('#map'), config.gmaps.options);
   setMapLocation(google);
+  createWispMarkers(tempSoundMarkers);
 });
