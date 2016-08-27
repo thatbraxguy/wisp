c5cf765ed5ac49730c0d98d481466647acf8b799
Ian <inhampson@gmail.com>
2016-08-27 15:19:36 -0700

    current maps progress.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+import GoogleMapLoader from 'google-maps';
+import config from './config';
 import db from './db';
 import wisps from './wispStore';
 
 require('../styles/main.scss');
 
 // init google maps
+alert('hello DannyBoi');
+console.log(config);
+GoogleMapLoader.KEY = config.gmaps.apiKey;
+GoogleMapLoader.load(google => {
+  new google.maps.Map(document.querySelector(".map"), config.gmaps.options);
+});
 
 // switch callback to instead create a node on the map!
 wisps.addListener('child_added', data => console.log(data));
