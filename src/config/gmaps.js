cd6fa7c52dd8d2e019a169f0db555e6471cba4ab
Ian <inhampson@gmail.com>
2016-08-27 16:52:36 -0700

    Disabled zoom, and pan. Added Ramda too.

--- src/config/gmaps.js
+++ src/config/gmaps.js
@@ -1,7 +1,11 @@
 export default{
   apiKey: 'AIzaSyBtgLcrGkY8cfMijUbD6fKIG0vD8OueYjA',
   options: {
+    zoom: 20,
     center: {lat: -34.397, lng: 150.644},
-    zoom: 20
+    disableDefaultUI: true,
+    draggable: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
   }
 };
