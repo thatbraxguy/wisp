ab75da213bdd96fc0ab642ee2435856d21b735e9
Peter Gyory <ptg3662@rit.edu>
2016-08-27 15:26:04 -0700

    Make map max screen size

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import wisps from './wispStore';
 require('../styles/main.scss');
 
 // init google maps
-alert('hello DannyBoi');
 console.log(config);
 GoogleMapLoader.KEY = config.gmaps.apiKey;
 GoogleMapLoader.load(google => {
-  new google.maps.Map(document.querySelector(".map"), config.gmaps.options);
+  new google.maps.Map(document.querySelector('#map'), config.gmaps.options);
 });
 
 // switch callback to instead create a node on the map!
