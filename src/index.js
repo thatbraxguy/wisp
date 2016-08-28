d31b59942dd74601a05c167b3240eaf04d4d0035
Peter Gyory <ptg3662@rit.edu>
2016-08-27 17:42:34 -0700

    Populate map based on firebase data

--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ require('../styles/main.scss');
 
 console.log(config);
 
-// basement coords.json = { latitude: 37.787487600000006, longitude: -122.3965566 }
-const tempSoundMarkers = [
-  { lat: 37.787487600000006, lng: -122.3965566, message: 'Everything was beautiful' },
-  { lat: 37.78748760000003, lng: -122.39658, message: 'And Nothing Hurt' }
-];
-location.init(config.gmaps)
-  .then(location.setMapLocation)
-  .then(() => location.createWispMarkers(tempSoundMarkers));
-
-
-// switch callback to instead create a node on the map!
-wisps.addListener('child_added', data => console.log(data));
-
 speech.record(x => speech.speak(x));
+
+location.init(config.gmaps)
+.then(location.setMapLocation)
+.then(() => {
+  wisps.addListener('child_added', location.createWispMarker);
+  wisps.init();
+});
